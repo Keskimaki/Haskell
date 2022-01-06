35540255c887408242a9096f12bf97bbe4cb4cf1
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-06 15:00:32 +0200

    Set1 ex9

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -110,7 +110,8 @@ isZero _ = False
 -- computes the sum 1+2+...+n
 
 sumTo :: Integer -> Integer
-sumTo = todo
+sumTo 1 = 1
+sumTo n = n + sumTo (n-1)
 
 ------------------------------------------------------------------------------
 -- Ex 10: power n k should compute n to the power k (i.e. n^k)
