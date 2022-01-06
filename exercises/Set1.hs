91fb1a262dbb15aa14381bf4ca75b77d41850262
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-06 14:50:22 +0200

    Set1 ex4

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -56,7 +56,8 @@ quadruple x = double (double x)
 --   distance 0 0 1 1  ==>  1.4142135...
 --   distance 1 1 4 5  ==>  5.0
 
-distance = todo
+distance :: Double -> Double -> Double -> Double -> Double
+distance x1 y1 x2 y2 = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
 
 ------------------------------------------------------------------------------
 -- Ex 5: define the function eeny that returns "eeny" for even inputs
