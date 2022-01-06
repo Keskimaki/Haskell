d3b16b68e17a47dfbd9446f7d7d968aa2171cfde
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-06 14:50:28 +0200

    Set1 ex5

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -66,7 +66,7 @@ distance x1 y1 x2 y2 = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
 -- Ps. have a look at the built in function "even"
 
 eeny :: Integer -> String
-eeny = todo
+eeny x = if even x then "eeny" else "meeny"
 
 ------------------------------------------------------------------------------
 -- Ex 6: here's the function checkPassword from the course material.
