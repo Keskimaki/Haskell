c7d7407bb492404171d564f964444f9559123eb3
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-06 14:49:59 +0200

    Set1 ex2

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -32,7 +32,7 @@ two = 2
 -- should take one argument and return it multiplied by two.
 
 double :: Integer -> Integer
-double x = todo
+double x = 2 * x
 
 ------------------------------------------------------------------------------
 -- Ex 3: define the function quadruple that uses the function double
