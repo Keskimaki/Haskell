8c11d62029fa27eccc5590a4ef87bde7c115724d
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-07 15:53:03 +0200

    Set3a ex13

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -263,7 +263,7 @@ multiCompose' fs x n = multiCompose' fs ((fs !! n) x) (n-1)
 --   multiApp id [head, (!!2), last] "axbxc" ==> ['a','b','c'] i.e. "abc"
 --   multiApp sum [head, (!!2), last] [1,9,2,9,3] ==> 6
 
-multiApp = todo
+multiApp f gs x = f (map ($ x) gs)
 
 ------------------------------------------------------------------------------
 -- Ex 14: in this exercise you get to implement an interpreter for a
