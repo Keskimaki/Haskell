6b70c526ccc9eba82c3122642d588ca14e874bce
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-07 14:08:11 +0200

    Set3a ex4

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -79,9 +79,11 @@ mapMaybe2 f (Just x) (Just y) = Just (f x y)
 palindromeHalfs :: [String] -> [String]
 palindromeHalfs xs = map firstHalf (filter palindrome xs)
 
-firstHalf = todo
+firstHalf :: String -> String
+firstHalf str = take (div (length str) 2 + mod (length str) 2) str
 
-palindrome = todo
+palindrome :: String -> Bool
+palindrome str = str == reverse str
 
 ------------------------------------------------------------------------------
 -- Ex 5: Implement a function capitalize that takes in a string and
