7a1b784f81bb0805e3c9d4eec3fcd7ff0d7d6303
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-09 16:07:36 +0200

    Set 5b ex1

--- exercises/Set5b.hs
+++ exercises/Set5b.hs
@@ -15,7 +15,8 @@ data Tree a = Empty | Node a (Tree a) (Tree a)
 -- because the tree might be empty (i.e. just a Empty)
 
 valAtRoot :: Tree a -> Maybe a
-valAtRoot t = todo
+valAtRoot Empty = Nothing
+valAtRoot (Node y _ _) = Just y
 
 ------------------------------------------------------------------------------
 -- Ex 2: compute the size of a tree, that is, the number of Node
