6f6cfceb4bc394a730f8fd0c7cf82e9a59687032
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-11 11:38:19 +0200

    Set6 ex6

--- exercises/Set6.hs
+++ exercises/Set6.hs
@@ -99,6 +99,13 @@ instance Price Milk where
 -- price [Just ChocolateEgg, Nothing, Just ChickenEgg]  ==> 50
 -- price [Nothing, Nothing, Just (Milk 1), Just (Milk 2)]  ==> 45
 
+instance Price a => Price (Maybe a) where
+  price Nothing = 0
+  price (Just i) = price i
+
+instance Price a => Price [a] where
+  price [] = 0
+  price (x:xs) = price x + price xs
 
 ------------------------------------------------------------------------------
 -- Ex 7: below you'll find the datatype Number, which is either an
