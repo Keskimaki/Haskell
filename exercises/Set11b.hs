c295ebd3d6bc542876edabb8ff080f6995d95ee4
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-13 15:02:38 +0200

    Set11b ex5

--- exercises/Set11b.hs
+++ exercises/Set11b.hs
@@ -123,7 +123,9 @@ compose op1 op2 c = do
 --   ["module Set11b where","","import Control.Monad"]
 
 hFetchLines :: Handle -> IO [String]
-hFetchLines = todo
+hFetchLines handle = do
+  content <- hGetContents handle
+  return (lines content)
 
 ------------------------------------------------------------------------------
 -- Ex 6: Given a Handle and a list of line indexes, produce the lines
