820945f759d2c66ed09617972fd1e1998eb96fca
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-13 13:54:06 +0200

    Set11a ex2

--- exercises/Set11a.hs
+++ exercises/Set11a.hs
@@ -32,7 +32,7 @@ hello = do
 -- argument and prints a line "HELLO name".
 
 greet :: String -> IO ()
-greet name = todo
+greet name = do putStrLn ("HELLO " ++ name)
 
 ------------------------------------------------------------------------------
 -- Ex 3: define the IO operation greet2 that reads a name from the
